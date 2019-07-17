3e782b43e22baf69dcb2cb621f8ab038ec587b8e
Aaron Schlesinger <aaron@ecomaz.net>
2019-07-17 14:08:47 -0700

    gophercon

--- main_handler.go
+++ main_handler.go
@@ -11,7 +11,7 @@ func mainHandler(c *gin.Context) {
 	<head></head>
 	<body>
 	<center>
-	<h1>Hey GopherCon EU!</h1>
+	<h1>Hey GopherCon!</h1>
 	<h3>What better way to demo Athens than with cat pictures?</h3>
 	<h3>A demo with cat <i>and</i> dog pictures</h3>
 	<p><a href="/kitty">Cats</a></p>
